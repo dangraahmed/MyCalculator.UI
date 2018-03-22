3692c3015d5d0fe506ede5355aed8e51a2aa8a08
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2018-03-22 20:15:57 +0530

    tax details view component added.

--- src/app/components/taxSlab/viewDetail/taxSlab.detail.view.component.ts
+++ src/app/components/taxSlab/viewDetail/taxSlab.detail.view.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs/Rx';
+
+import * as fromStore from '../../../modules/shared/ngrx/index';
+import { TaxSlab } from '../../../modules/user/taxSlab/actions/index';
+import { ITaxSlab } from '../../../modules/user/taxSlab/index';
+
 
 @Component({
     moduleId: module.id,
@@ -6,12 +13,33 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'taxSlab.detail.view.component.html',
     styleUrls: ['taxSlab.detail.view.component.scss']
 })
-export class TaxSlabViewDetailComponent implements OnInit {
-
-    constructor() { }
-  
-    ngOnInit() {
+export class TaxSlabViewDetailComponent implements OnInit, OnDestroy {
+    public taxSlab: ITaxSlab;
+    private _ngUnsubscribe: Subject<void> = new Subject<void>();
+    private selectedId: number;
+    constructor(
+        private store: Store<fromStore.IAppState>) {
     }
-  
-  }
-  
+
+    ngOnInit() {
+        this.store.select(fromStore.getSelectedTaxSlabsId)
+        .takeUntil(this._ngUnsubscribe)
+        .subscribe(selectedTaxSlabsId => {
+            this.selectedId = selectedTaxSlabsId;
+        });
+
+        this.store.select(fromStore.getSelectedTaxSlab)
+            .takeUntil(this._ngUnsubscribe)
+            .subscribe(taxSlab => {
+                if (!taxSlab.taxSlabDetail) {
+                    this.store.dispatch(new TaxSlab.LoadTaxSlabDetailAction(this.selectedId));
+                }
+                this.taxSlab = taxSlab;
+            });
+    }
+
+    ngOnDestroy() {
+        this._ngUnsubscribe.next();
+        this._ngUnsubscribe.complete();
+    }
+}
